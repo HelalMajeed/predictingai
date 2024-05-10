0ff4f8acc30e9edb844ffda1a17b722348374270
Your Name <your.email@example.com>
2024-05-11 01:55:40 +0300

    edit ui

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const axios = require('axios');
 
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
 
 app.listen(PORT, () => {
         console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
+
+////////////////// api here //////////////////////////
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        const data = response.data;
+        res.render('index', { data }); 
+        res.json(data);
+        // res.send(data)
+        console.log(response);
+        // console.log("hello");
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Error fetching data' });
+    }
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,137 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttonContainer = document.getElementById('buttonContainer');
-    const subButtonContainer = document.getElementById('subButtonContainer');
-    const searchBar = document.getElementById('search');
-    const searchButtonContainer = document.getElementById('searchButtonContainer');
-    let searchButton = null;
-    var active = 0;
-    buttonContainer.addEventListener('click', function(event) {
-        if (event.target.classList.contains('category-button')) {
-            const clickedButton = event.target;
-            const category = clickedButton.getAttribute('data-category');
-            showSubButtons(category);
-            updateSearchBarStyle(category);
-            toggleActiveCategory(clickedButton);
-            updateSearchBarState();
-        }
-    });
-    subButtonContainer.addEventListener('click', function(event) {
-        if (event.target.classList.contains('sub-category-button')) {
-            toggleActiveButton(event.target);
-            updateSearchBarState();
-        }
-    });
-    function showSubButtons(category) {
-        subButtonContainer.innerHTML = '';
-        let subButtons = [];
-        switch (category) {
-            case 'sport':
-                subButtons = ['Football', 'Basketball'];
-                break;
-            case 'exam':
-                subButtons = ['GRE', 'TOEFL', 'IELTS', 'SAT', 'ACT'];
-                break;
-            case 'study':
-                subButtons = ['Math', 'Computer Scince', 'Physics'];
-                break;
-            default:
-                subButtons = [];
-                break;
-        }
-        subButtons.forEach(buttonText => {
-            const button = createButton(buttonText, 'sub-category-button');
-            subButtonContainer.appendChild(button);
-        });
-    }
-    function updateSearchBarStyle(category) {
-        switch (category) {
-            case 'sport':
-                searchBar.style.backgroundColor = '#007bff43'; 
-                searchBar.style.color = '#fff';
-                break;
-            case 'study':
-                searchBar.style.backgroundColor = '#28a745'; 
-                searchBar.style.color = '#fff';
-                break;
-            case 'exam':
-                searchBar.style.backgroundColor = '#ffee005f'; 
-                searchBar.style.color = '#fff';
-                break;
-            case 'weather':
-                searchBar.style.backgroundColor = '#ff00005f';
-                searchBar.style.color = '#212529'; 
-                break;
-            default:
-                searchBar.style.backgroundColor = '';
-                searchBar.style.color = '';
-                break;
-        }
-    }
-    function toggleActiveCategory(clickedButton) {
-        const categoryButtons = buttonContainer.querySelectorAll('.category-button');
-        categoryButtons.forEach(button => {
-            if (button === clickedButton) {
-                button.classList.add('active');
-                searchBar.style.display = "none";
-            } else {
-                button.classList.remove('active');
-                searchBar.style.display = "none";
-            }
-        });
-        updateSearchBarState();
-    }
+const textButton = document.getElementById('text');
+const imageButton = document.getElementById('image');
+const searchContainer = document.getElementById('search');
+const searchButton = document.getElementById('searchButton');
 
+function activateButton(clickedButton, otherButton) {
+    clickedButton.classList.add('active');
+    otherButton.classList.remove('active');
+}
 
-    function toggleActiveButton(clickButton) {
-        subButtonContainer.classList.toggle('active');
-        if (subButtonContainer.classList.contains('active')) {
-            searchBar.style.display = "flex";
-            searchBar.style.justifyContent = "center";
-        } else {
-            searchBar.style.display = "none";
-        }
-        ///////////////// sub button activity here ////////////////////////
-        const isActive = clickButton.classList.contains('active');
-        if (isActive) {
-            clickButton.classList.remove('active');
-            console.log('not active')
-        } else {
-            clickButton.classList.add('active');
-
-            console.log('active')
-        }
-        console.log(isActive);
-        updateSearchBarState();
-    }
-
-    searchBar.addEventListener('input', function () {
-        updateSearchBarState();
-    });
-    function updateSearchBarState() {
-        const searchValue = searchBar.value.trim(); 
-        const searchButtonContainer = document.getElementById('searchButtonContainer');
-
-        if (searchValue !== '') {
-            if (!searchButton) {
-                searchButton = createButton('Search', 'search-button');
-                searchButtonContainer.appendChild(searchButton);
-            }
-            searchButton.style.display = 'flex'; 
-            searchButton.style.justifyContent = 'center'; 
-        } else {
-            // if searchValue is empty, hide the search button
-            if (searchButton) {
-                searchButtonContainer.removeChild(searchButton);
-                searchButton = null;
-            }
-        }
-    }
-
-    function createButton(text, className) {
-        const button = document.createElement('button');
-        button.textContent = text;
-        button.type = 'button';
-        button.classList.add(className);
-        return button;
-    }
+textButton.addEventListener('click', (event) => {
+    console.log('Text Button Clicked');
+    activateButton(textButton, imageButton); 
+    searchContainer.style.display = 'block'; 
 });
+
+imageButton.addEventListener('click', (event) => {
+    console.log('Image Button Clicked');
+    activateButton(imageButton, textButton); 
+    searchContainer.style.display = 'block'; 
+});
+
+function toggleSearchButton() {
+    if (searchContainer.value.trim() === '') {
+        searchButton.style.display = "none";
+    } else {
+        searchButton.style.display = "block";
+    }
+}
+
+searchContainer.addEventListener('input', (event) => {
+    toggleSearchButton();
+})
+
+fetch('http://localhost:3000/')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        console.log('Response status:', error.response.status);
+        console.log('Response text:', error.response.statusText);
+    });

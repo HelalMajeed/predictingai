bc980948aa150c959b42083c13c3f2f43b5759bf
Your Name <your.email@example.com>
2024-05-15 22:54:30 +0300

    update ui

---
+++ script.js
@@ -0,0 +1,33 @@
+const textButton = document.getElementById('text');
+const imageButton = document.getElementById('image');
+const searchContainer = document.getElementById('search');
+const searchButton = document.getElementById('searchButton');
+
+function activateButton(clickedButton, otherButton) {
+    clickedButton.classList.add('active');
+    otherButton.classList.remove('active');
+}
+
+textButton.addEventListener('click', (event) => {
+    console.log('Text Button Clicked');
+    activateButton(textButton, imageButton); 
+    searchContainer.style.display = 'block'; 
+});
+
+imageButton.addEventListener('click', (event) => {
+    console.log('Image Button Clicked');
+    activateButton(imageButton, textButton); 
+    searchContainer.style.display = 'block'; 
+});
+
+function toggleSearchButton() {
+    if (searchContainer.value.trim() === '') {
+        searchButton.style.display = "none";
+    } else {
+        searchButton.style.display = "block";
+    }
+}
+
+searchContainer.addEventListener('input', (event) => {
+    toggleSearchButton();
+})

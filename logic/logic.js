56b805ab1082cf9635a3fccb2fcb30817e341350
Crescent <90155421+HelalMajeed@users.noreply.github.com>
2024-05-30 00:07:50 +0300

    Update logic.js

--- logic/logic.js
+++ logic/logic.js
@@ -3,20 +3,10 @@ function moveBG() {
   logo.classList.toggle("move-up");
 }
 
-setTimeout(() => {
-  const loader = document.querySelector(".loader");
-  if (loader) {
-    loader.remove();
-  }
-  searchButton.style.display = "block";
-}, 10000);
 
 function showResult() {
   wrapper.style.display = 'flex'; 
   result.classList.add('animate'); 
-  const loader = document.createElement("div");
-  loader.className = "loader";
-  wrapper.appendChild(loader);
   setTimeout(() => {
     const loader = document.querySelector(".loader");
     if (loader) {
